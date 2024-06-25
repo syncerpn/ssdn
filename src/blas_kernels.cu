7879713a669dcd742be268b9c91c1b65dedc2685
syncerpn <nghianteeek57@gmail.com>
2024-06-25 16:13:26 +0900

    Update blas_kernels.cu

--- src/blas_kernels.cu
+++ src/blas_kernels.cu
@@ -9,14 +9,12 @@
 
 #include <assert.h>
 
-__global__ void fill_kernel(int N, float ALPHA, float *X, int INCX)
-{
+__global__ void fill_kernel(int N, float ALPHA, float *X, int INCX) {
     int i = (blockIdx.x + blockIdx.y*gridDim.x) * blockDim.x + threadIdx.x;
     if(i < N) X[i*INCX] = ALPHA;
 }
 
-extern "C" void fill_gpu(int N, float ALPHA, float * X, int INCX)
-{
+void fill_gpu(int N, float ALPHA, float * X, int INCX) {
     fill_kernel<<<cuda_gridsize(N), BLOCK>>>(N, ALPHA, X, INCX);
     check_error(cudaPeekAtLastError());
 }
