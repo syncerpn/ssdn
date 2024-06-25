9e6ced70f03869d7e4154d520eebe4c2552d6b83
syncerpn <nghianteeek57@gmail.com>
2024-06-25 15:17:04 +0900

    Update ssdn.cpp

--- src/ssdn.cpp
+++ src/ssdn.cpp
@@ -1,7 +1,7 @@
 #include "ssdn.h"
 
 int main() {
-	float* x = calloc(100, sizeof(float));
+	float* x = (float*)calloc(100, sizeof(float));
 	float* x_gpu = cuda_make_array<float>(x, 100);
 	cuda_free<float>(x_gpu);
 	free(x);
