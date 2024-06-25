42bcbf16e6f086090da83db8e7d3f1fe94dfafa8
syncerpn <nghianteeek57@gmail.com>
2024-06-25 15:56:02 +0900

    ups

--- include/ssdn.h
+++ include/ssdn.h
@@ -15,40 +15,4 @@ extern unsigned int seed;
 
 void cuda_set_device(int n);
 
-template <typename T>
-T *cuda_make_array(T *x, size_t n) {
-    T *x_gpu;
-    size_t size = sizeof(T) * n;
-    cudaError_t status = cudaMalloc((void **)&x_gpu, size);
-    check_error(status);
-    if (x) {
-        status = cudaMemcpy(x_gpu, x, size, cudaMemcpyHostToDevice);
-        check_error(status);
-    } else {
-        fill_gpu(n, 0, x_gpu, 1);
-    }
-    if (!x_gpu) error("Cuda malloc failed\n");
-    return x_gpu;
-}
-
-template <typename T>
-void cuda_free(T *x_gpu) {
-    cudaError_t status = cudaFree(x_gpu);
-    check_error(status);
-}
-
-template <typename T>
-void cuda_push_array(T *x_gpu, T *x, size_t n) {
-    size_t size = sizeof(T) * n;
-    cudaError_t status = cudaMemcpy(x_gpu, x, size, cudaMemcpyHostToDevice);
-    check_error(status);
-}
-
-template <typename T>
-void cuda_pull_array(T *x_gpu, T *x, size_t n) {
-    size_t size = sizeof(T) * n;
-    cudaError_t status = cudaMemcpy(x, x_gpu, size, cudaMemcpyDeviceToHost);
-    check_error(status);
-}
-
 #endif
