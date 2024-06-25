2a59bb191eab3381f2423780d10a6fa48c78309a
syncerpn <nghianteeek57@gmail.com>
2024-06-25 13:49:01 +0900

    ups

--- include/ssdn.h
+++ include/ssdn.h
@@ -1,2 +1,14 @@
 #define BLOCK 512
 extern int gpu_index;
+
+#include "cuda_runtime.h"
+#include "curand.h"
+#include "cublas_v2.h"
+
+#include "cudnn.h"
+
+void cuda_set_device(int n);
+template <typename T> T *cuda_make_array(T *x, size_t n);
+template <typename T> void cuda_free(T *x_gpu);
+template <typename T> void cuda_push_array(T *x_gpu, T *x, size_t n);
+template <typename T> void cuda_pull_array(T *x_gpu, T *x, size_t n);
---
+++ src/_cuda.h
@@ -0,0 +1,15 @@
+#ifndef CUDA_H
+#define CUDA_H
+
+#include "ssdn.h"
+#define WARP_SIZE 32
+
+void check_error(cudaError_t status);
+dim3 cuda_gridsize(size_t n);
+cudnnHandle_t cudnn_handle();
+cublasHandle_t blas_handle();
+void cuda_random(float *x_gpu, size_t n);
+cudaStream_t get_cuda_stream();
+int get_number_of_blocks(int array_size, int block_size);
+
+#endif
