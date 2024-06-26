07f85e07317fcdbe3451eb1f3fa84a52f8124fe1
syncerpn <nghianteeek57@gmail.com>
2024-06-26 12:43:52 +0900

    Update ssdn.cpp

--- src/ssdn.cpp
+++ src/ssdn.cpp
@@ -1,8 +1,29 @@
+#include <iostream>
+
 #include "ssdn.h"
 
+void lead_one_encode(int x, int &k, int &p) {
+	if (x == 0 || x == 1) {
+		k = 0;
+		p = 0;
+	}
+	k = 0;
+	while (x > 1) {
+		x = x >> 1;
+		k += 1;
+	}
+	p = x - (1 << k);	
+}
+
+void run_sim_fast_approx_ma() {
+
+}
+
 int main() {
-	float* x = (float*)calloc(100, sizeof(float));
-	float* x_gpu = cuda_make_array(x, 100);
-	cuda_free(x_gpu);
-	free(x);
+	int k, p;
+	for (int i = 0; i <= 512; ++i) {
+		lead_one_encode(i, k, p);
+		std::cout << i << " " << k << " " << p << std::endl;
+	}
+	return 0;
 }
