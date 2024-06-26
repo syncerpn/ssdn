f770807c00de0ff01cec02b88b31f347a6310f40
syncerpn <nghianteeek57@gmail.com>
2024-06-26 13:00:27 +0900

    Update ssdn.cpp

--- src/ssdn.cpp
+++ src/ssdn.cpp
@@ -16,6 +16,25 @@ void lead_one_encode(int x, int &k, int &p) {
 	p = x - (1 << k);
 }
 
+void approximate(int i, int j) {
+	if (i == 0 || j == 0) return 0;
+
+	int sign_i = i > 0 ? 1 : -1;
+	int sign_j = j > 0 ? 1 : -1;
+
+	i = sign_i * i;
+	j = sign_j * j;
+
+	int ki, pi, kj, pj;
+	lead_one_encode(i, ki, pi);
+	lead_one_encode(j, kj, pj);
+
+	int p1 = pi * pj;
+	int p2 = ((1 << ki) - pi) * ((1 << kj) - pj);
+	int e = p1 < p2 ? p1 : p2;
+	return sign_i * sign_j * (i * j - e);
+}
+
 void run_sim_fast_approx_ma() {
 
 }
@@ -23,8 +42,9 @@ void run_sim_fast_approx_ma() {
 int main() {
 	int k, p;
 	for (int i = 0; i <= 512; ++i) {
-		lead_one_encode(i, k, p);
-		std::cout << i << " " << k << " " << p << std::endl;
+		for (int j = i; j <= 512; ++j) {
+			std::cout << i << " x " << j << " = " << approximate(i, j) << std::endl;
+		}
 	}
 	return 0;
 }
