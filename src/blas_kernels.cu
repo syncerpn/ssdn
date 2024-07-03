805ed5e1af31e5c591c097bc32c021b8d4474509
syncerpn <nghianteeek57@gmail.com>
2024-07-03 14:39:17 +0900

    ups

--- src/blas_kernels.cu
+++ src/blas_kernels.cu
@@ -321,13 +321,15 @@ void compensate_wp_gpu(int N, float p, float *X, int INCX, float *Y, int INCY) {
 __global__ void quantize_compensate_wp_kernel(int N, float step, int nbit, bool sign, float *X, int INCX, float *Y, int INCY) {
     int i = (blockIdx.x + blockIdx.y*gridDim.x) * blockDim.x + threadIdx.x;
     if (i >= N) return;
+    float sign_m = 1.0 if X[i*INCX] > 0 else -1.0;
     float pos_end = sign ?  (float)(1 << (nbit - 1)) - 1 : (1 << nbit) - 1;
     float neg_end = sign ? -(float)(1 << (nbit - 1))     : 0;
-    float raw_q = ceilf(X[i*INCX] / step);
-    int Xi = (int)(abs(raw_q));
+    float raw_q = ceilf(abs(X[i*INCX]) / step);
+    int Xi = (int)(raw_q);
     if (Xi & (Xi - 1) != 0) {
-        raw_q = roundf(raw_q + 0.048 * raw_q);
+        raw_q += roundf(0.048 * raw_q);
     }
+    raw_q = raw_q * sign_m;
     Y[i*INCY] = raw_q > pos_end ? pos_end : (raw_q < neg_end ? neg_end : raw_q);
 }
 
