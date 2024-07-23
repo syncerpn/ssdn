1b74ae73296ce2aaa44681e20cc1258f58b85764
syncerpn <nghianteeek57@gmail.com>
2024-07-23 14:17:58 +0900

    ups

--- python/ssdn.py
+++ python/ssdn.py
@@ -47,9 +47,10 @@ fill_gpu.restype = None
 
 if __name__ == "__main__":
 	cuda_set_device(0)
-	f = (c_float*10)(list(range(10)))
+	f = (c_float*10)(1,2,3,4,5,6,7,8,9,0)
 	print(f)
 	c = cuda_make_array(pointer(f), 10)
 	fill_gpu(3, 20, c, 1)
 	cuda_pull_array(c, pointer(f), 10)
 	print(f)
+	cuda_free(c)
